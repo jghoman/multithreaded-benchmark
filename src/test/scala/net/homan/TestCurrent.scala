479418f149c92089f3e7ff79cc8c6e73d6ef3361
Jakob Homan <jghoman@gmail.com>
2013-04-23 14:17:28 -0700

    Initial commit

---
+++ src/test/scala/net/homan/TestCurrent.scala
@@ -0,0 +1,99 @@
+package net.homan
+
+import org.junit.Test
+import org.junit.Assert._
+import java.util.Random
+import java.util.concurrent.{TimeUnit, CountDownLatch, LinkedBlockingDeque}
+
+class TestCurrent {
+  type Work = (Int, Int)
+  val DIVISOR = 10
+  val NUM_PIECES_OF_WORK_PER_WORKER = 10000 / DIVISOR
+  val NUM_WORKERS = 2000 / DIVISOR
+  val QUEUE_SIZE = 1000 / DIVISOR
+  @Test
+  def current = {
+    println("NUM_PIECES_OF_WORK_PER_WORKER = %d, NUM_WORKERS = %d, QUEUE_SIZE = %d" format (NUM_PIECES_OF_WORK_PER_WORKER, NUM_WORKERS, QUEUE_SIZE))
+    // Some busy worker, but not so busy that the JVM optimizes it away
+    class Worker() {
+      val MS_TO_WAIT = 1
+      val r = new Random(Thread.currentThread().hashCode())
+      var workProcessed = 0
+      def work(i:Int):Int = {
+        val stop = System.currentTimeMillis + MS_TO_WAIT
+        var j = i
+        while(System.currentTimeMillis() < stop) {
+          j = j + i + (r.nextInt() % 1000)
+        }
+
+        workProcessed = workProcessed + 1
+        j
+      }
+    }
+
+    // Build a bunch of workers, to be called upon when their id randomly arrives
+    val range = 0 until NUM_WORKERS
+    val workers = range.map(r => (r, new Worker)).toMap
+
+    // Produce some work - confusing name, eh? - and distribute it to some worker
+    // via the concurrent queue
+    // Do this NUM_PIECES_OF_WORK_TIMES.  Wait for the signal before beginning
+    abstract class Consumer(latch:CountDownLatch) extends Runnable {
+      var workCompleted = 0
+      val r = new Random(Thread.currentThread().hashCode())
+      val work = (0 until NUM_PIECES_OF_WORK_PER_WORKER).map(n => (n % NUM_WORKERS, n)).toList
+
+      def run() {
+        latch.await()
+        work.foreach(w => getQueue.put(w))
+      }
+
+      def getQueue:LinkedBlockingDeque[Work]
+    }
+
+    println("Creating blocking queue with %d capacity" format QUEUE_SIZE)
+    val queue = new LinkedBlockingDeque[Work](QUEUE_SIZE)
+    val latch = new CountDownLatch(1)
+    val consumers = range.map(r => new Thread(new Consumer(latch) {
+          def getQueue: LinkedBlockingDeque[TestCurrent.this.type#Work] = queue
+      })).toList
+    println("Created %d consumers to create some work." format consumers.size)
+
+    // Main thread that polls the queue and hands out the work to the workers
+    val runner = new Thread(new Runnable {
+      val totalPiecesOfWorker = NUM_PIECES_OF_WORK_PER_WORKER * NUM_WORKERS
+      var workReceived = 0
+      var bigSum = 0
+      var timesQueueWasFull = 0
+        def run() {
+
+          println("Starting...")
+          while(workReceived < totalPiecesOfWorker) {
+            if (queue.size() == QUEUE_SIZE) timesQueueWasFull = timesQueueWasFull + 1
+            val (worker, work) = queue.poll(1, TimeUnit.SECONDS)
+
+            bigSum = bigSum + workers(worker).work(work)
+            workReceived = workReceived + 1
+          }
+
+          println("All done! Received %d pieces of work. bigSum = %d and queue was full %d times" format (workReceived, bigSum, timesQueueWasFull))
+        }
+      })
+    runner.setName("Runner thread")
+    runner.start
+
+    consumers.foreach(_.start)
+
+    // And go!
+    val start = System.currentTimeMillis()
+    latch.countDown
+    runner.join()
+    val stop = System.currentTimeMillis() - start
+    consumers.foreach(c => if (!c.isAlive) c.join)
+    println("Total pieces of work processed = " + workers.values.foldLeft(0)(_ + _.workProcessed))
+    println("Done with runner, elapsed time = %d milliseconds (%1.2f seconds)." format (stop, stop / 1000.0))
+
+    assertEquals(0, queue.size())
+  }
+
+}
---
+++ src/test/scala/net/homan/TestVeryParallel.scala
@@ -0,0 +1,108 @@
+package net.homan
+
+import org.junit.Test
+import java.util.Random
+import java.util.concurrent.{CountDownLatch, TimeUnit, LinkedBlockingQueue}
+import scala.collection._
+
+class TestVeryParallel {
+  @Test
+  def parallel = {
+    type Work = (Int, Int)
+    val DIVISOR = 10
+    val NUM_PIECES_OF_WORK_PER_WORKER = 10000 / DIVISOR
+    val NUM_WORKERS = 2000 / DIVISOR
+    val QUEUE_SIZE = 1000 / DIVISOR
+    val NUM_CONSUMERS = 5
+    // Some busy worker, but not so busy that the JVM optimizes it away
+    class Worker(latch:CountDownLatch) extends Runnable {
+      val MS_TO_WAIT = 1
+      val WORKER_QUEUE_SIZE = 10
+      val r = new Random(Thread.currentThread().hashCode())
+      val workQueue = new LinkedBlockingQueue[Int](WORKER_QUEUE_SIZE)
+      var bigSum = 0
+      var workProcessed = 0
+      @volatile var keepGoing = true
+
+      def work(i:Int):Int = {
+        val stop = System.currentTimeMillis + MS_TO_WAIT
+        var j = i
+        while(System.currentTimeMillis() < stop) {
+          j = j + i + (r.nextInt() % 1000)
+        }
+        j
+      }
+
+      def run() {
+        latch.await
+        while(keepGoing || workQueue.size > 0) {
+          val wNull = workQueue.poll(1, TimeUnit.SECONDS)
+
+          if (wNull != null) {
+            bigSum = bigSum + work(wNull)
+            workProcessed = workProcessed + 1
+          }
+
+        }
+
+      }
+    }
+
+    val latch = new CountDownLatch(1)
+    val workers = (0 until NUM_WORKERS).map(w => (w, new Worker(latch))).toMap
+    val workersThreads = workers.values.map(w => new Thread(w))
+
+    val work = (for {i <- 0 until NUM_WORKERS
+                     j <- 0 until NUM_PIECES_OF_WORK_PER_WORKER
+                    } yield (i % NUM_WORKERS, j)).toList
+
+    println("Work size = " + work.size)
+
+    //val workQueues = (0 until NUM_CONSUMERS).map(new mutable.ListBuffer[Work])
+    val workQueues = Array.fill(NUM_CONSUMERS) { new mutable.ListBuffer[Work]}
+    // Distribute the work to all of the work queues
+    var counter:Int = 0
+    for (w <- work) {
+      workQueues(counter % NUM_CONSUMERS) += w
+      counter = counter + 1
+    }
+
+    for(wq <- workQueues) { println("workqueue size = %d" format wq.size )}
+
+    class Consumer(workers:Map[Int, Worker], latch:CountDownLatch, wq:List[Work]) extends Runnable {
+      var count = 0
+      def run() {
+        latch.await
+
+        for (w <- wq) {
+          val worker:Int = w._1
+          val work:Int = w._2
+          //println("Sending %d to worker %d" format (work, worker))
+          workers.get(worker).get.workQueue.put(work)
+          count = count + 1
+        }
+      }
+    }
+
+    val consumers = (0 until NUM_CONSUMERS).map(c => new Thread(new Consumer(workers, latch, scala.util.Random.shuffle(workQueues(c)).toList), "Consumer " + c))
+    workersThreads.foreach(_.start)
+    consumers.foreach(_.start)
+
+    // And go!
+    val start = System.currentTimeMillis()
+    latch.countDown()
+
+    consumers.foreach(_.join)
+
+    workers.values.foreach(_.keepGoing = false)
+    workersThreads.foreach(_.join)
+    val stop = System.currentTimeMillis() - start
+    println("All workers joined")
+
+    val totalWorkProcessed = workers.values.foldLeft(0)(_ + _.workProcessed)
+    println("Total pieces of work processed = " + totalWorkProcessed)
+    println("Done with parallel work, elapsed time = %d milliseconds (%1.2f seconds)." format (stop, stop / 1000.0))
+    println("-30-")
+  }
+
+}
